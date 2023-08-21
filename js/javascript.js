b54288261fb2b5ba37967341134b54c0b5e728f3
safuad <dipusaroar@gmail.com>
2023-08-21 16:00:31 +0200

    first commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,12 @@
 const applyButton = document.getElementById('apply-button')
 const couponInput = document.getElementById('coupon-code')
+const buttonPurchase = document.getElementById('btn-purchase')
+const buttonGoHome = document.getElementById('btn-go-home')
+const selectedItemsContainer = document.getElementById('selected-items');
+const firstTotal = document.getElementById('total-price');
+const discountAmountTag = document.getElementById('discount');
+const findTotalField = document.getElementById('total');
+const buttonSell = document.getElementById('btn-sell')
 
 let total = 0
 
@@ -16,18 +23,24 @@ function handelClick(target){
     total = total + price
 
     const firstTotal = document.getElementById('total-price')
-    firstTotal.innerText = total.toString()
+    firstTotal.innerText = total.toFixed(2).toString()
 
     
     
     const findTotalField = document.getElementById('total')
-    findTotalField.innerText = total
+    findTotalField.innerText = total.toFixed(2)
     
     if (total >= 200){
         applyButton.removeAttribute('disabled')
+        buttonSell.removeAttribute('disabled')
+        
     }
     // else{applyButton.setAttribute('disabled')}
 
+    if (total >= 1){
+        buttonPurchase.removeAttribute('disabled')
+    }
+    
 }
 
 applyButton.addEventListener('click', function () {
@@ -37,17 +50,33 @@ applyButton.addEventListener('click', function () {
         const discount = total * 0.20; 
 
         const discountAmountTeg = document.getElementById('discount') 
-        discountAmountTeg.innerText = discount
+        discountAmountTeg.innerText = discount.toFixed(2)
 
         total = total - discount;
 
-
-
         const firstTotal = document.getElementById('total');
-        firstTotal.innerText = total.toString();
+        firstTotal.innerText = total.toFixed(2).toString();
     }
 });
 
 
+buttonGoHome.addEventListener('click', resetPage);
 
+function resetPage() {
+    total = 0;
+    selectedItemsContainer.innerText = '';
+    firstTotal.innerText = ' 00';
+    discountAmountTag.innerText = ' 00';
+    findTotalField.innerText = ' 00';
+    applyButton.setAttribute('disabled', 'disabled');
+    buttonPurchase.setAttribute('disabled', 'disabled');
+    couponInput.value = ''; // Reset the coupon input field
+}
 
+buttonSell.addEventListener('click',function() {
+   
+    const buttonInnerText = buttonSell.innerText;
+    
+    couponInput.value = buttonInnerText
+
+})
